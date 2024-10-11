024e937a5c8715c9c75db17a32a4660b5ec5b2fe
Ren <reine.donnestad@gmail.com>
2024-10-11 17:43:40 -0400

    added draft comment section

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -1,26 +1,46 @@
-const div = document.createElement('div');
+const allShows = document.querySelector('.shows');
 
-// create a p object with some text content in it
-const date = document.createElement('p');
-date.textContent = 'DATE';
+const showData = [
+    {date: 'Mon Sept 09 2024', venue: 'Ronald Lane', location: 'San Francisco'},
+    {date: 'Tues Sept 17 2024', venue: 'Pier 3 East', location: 'San Francisco'},
+    {date: 'Sat Oct 12 2024', venue: 'View Lounge', location: 'San Francisco'},
+    {date: 'Sat Nov 16 2024', venue: 'Hyatt Agency', location: 'San Francisco'},
+    {date: 'Fri Nov 29 2024', venue: 'Moscow Center', location: 'San Francisco'},
+    {date: 'Wed Dec 18 2024', venue: 'Press Club', location:'San Francisco'},
+];
+
+for (let i=0; i < showData.length; i++) {
+    const showContainer = document.createElement('div');
+    showContainer.classList.add('show-container');
+
+    // create a p object with some text content in it
+    const date = document.createElement('p');
+    date.classList.add('date');
+    date.textContent = showData[i].date;
+
+    const venue = document.createElement('p');
+    venue.classList.add('venue');
+    venue.textContent = showData[i].venue;
+
+    const showLocation = document.createElement('p');
+    showLocation.classList.add('location');
+    showLocation.textContent = showData[i].location;
+
+    const button = document.createElement("button");
+        button.innerText = "BUY TICKETS";
+
+    showContainer.appendChild(date);
+    showContainer.appendChild(venue);
+    showContainer.appendChild(showLocation);
+    showContainer.appendChild(button);
 
 
-const venue = document.createElement('p');
-venue.textContent = 'VENUE';
+button.addEventListener('click',()=> {
+    console.log('someone bought a ticket');
+});
 
-const location = document.createElement('p');
-location.textContent = 'LOCATION';
+allShows.appendChild(showContainer);
+}
 
+console.log(allShows);
 
-// put the p object inside the div object
-div.appendChild(date);
-div.appendChild(venue);
-div.appendChild(location);
-
-// let's see how our div object looks like
-console.log(div);
-
-const container = document.getElementById('container');
-
-// show me the div!
-container.appendChild(div);
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,5 +1,88 @@
-// //1. Access the form.
-// const form = document.getElementById('inputComment');
+const commentEntries = [
+    {name: "Victor Pinto", date:"11/02/2023", comment:"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."},
+    {name:"Christina Cabrera", date: "10/28/2023", comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."},
+    {name:"Isaac Tadesse", date: "10/20/2023", comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."}
+]
+
+console.log(commentEntries);
+
+function createCommentCard(entry) {
+    const cardElement = document.createElement("article");
+    cardElement.classList.add("comment__card");
+
+    const pTag = document.createElement("p");
+    pTag.classList.add("comment__card--name");
+    pTag.innerText = entry.name;
+
+    const dateElement = document.createElement("span");
+    dateElement.classList.add("comment__card--date");
+    dateElement.innerText = entry.date;
+
+    const commentElement = document.createElement("p");
+    pTag.classList.add("comment__card--comment");
+    pTag.innerText = entry.comment;
+
+    cardElement.appendChild(pTag);
+    cardElement.appendChild(dateElement);
+    cardElement.appendChild(commentElement);
+
+    console.log(cardElement);
+    return cardElement;
+}
+// createCommentCard({});
+
+let renderCommentCards = () => {
+    const myCommentsEl = document.querySelector(".comment__card--comment");
+    myCommentsEl.innerHTML ="";
+
+    for (let i=0; i < commentEntries.length; i++){
+        const card = createCommentCard(commentEntries[i]);
+        myCommentsEl.append(card);
+    }
+};
+
+renderCommentCards();
+
+const formEl = document.querySelector(".commentforum");
+formEl.addEventListener("submit",(event) => {
+    event.preventDefault();
+
+    let cardData = {
+        name: event.target.name.value,
+        date:event.target.date.value,
+        comment:event.target.comment.value,
+    };
+commentEntries.push(cardData);
+console.log(commentEntries);
+
+renderCommentCards();
+
+});
+
+
+// //this is to get the button to click
+// document.getElementById('commentForm').addEventListener('submit', function(event){
+//     event.preventDefault();
+
+//     const name = document.getElementById('inputName').value;
+//     const comment = document.getElementById('inputComment').value;
+
+//     console.log("Name:", name);
+//     console.log("Comment:", comment);
+
+//     const outputEntry = document.getElementById('outputEntry');
+//     const newComment = document.createElement('p');
+//     newComment.innerHTML = `<strong>${name}:</strong> ${comment}`;
+
+//     console.log("New Comment Element:", newComment);
+//     console.log("Output Element:", outputEntry);
+
+//     outputEntry.appendChild(newComment);
+
+//     document.getElementById('commentForm').reset();
+
+// });
+
 
 // //2. Get values from the form. 
 // form.addEventListener('submit',function(e){

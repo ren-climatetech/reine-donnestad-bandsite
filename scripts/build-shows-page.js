b4a7ded1706527dcf367d7214415a463830ff5cf
Ren <reine.donnestad@gmail.com>
2024-10-13 19:45:22 -0400

    formatted JavaScript for Shows page

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -9,38 +9,68 @@ const showData = [
     {date: 'Wed Dec 18 2024', venue: 'Press Club', location:'San Francisco'},
 ];
 
+
+
 for (let i=0; i < showData.length; i++) {
     const showContainer = document.createElement('div');
     showContainer.classList.add('show-container');
 
-    // create a p object with some text content in it
+    const dateContainer = document.createElement('div');
+    dateContainer.classList.add('date-container');
+
+    const headerDate = document.createElement('p');
+    headerDate.classList.add('shows__header');
+    headerDate.textContent = 'DATE';
+
     const date = document.createElement('p');
     date.classList.add('date');
     date.textContent = showData[i].date;
+
+    dateContainer.appendChild(headerDate);
+    dateContainer.appendChild(date);
+
+    const venueContainer = document.createElement('div');
+    venueContainer.classList.add('venue-container');
+
+    const headerVenue = document.createElement('p');
+    headerVenue.classList.add('shows__header');
+    headerVenue.textContent = 'VENUE';
 
     const venue = document.createElement('p');
     venue.classList.add('venue');
     venue.textContent = showData[i].venue;
 
+    venueContainer.appendChild(headerVenue);
+    venueContainer.appendChild(venue);
+
+    const locationContainer = document.createElement('div');
+    locationContainer.classList.add('location-container');
+
+
+    const headerLocation = document.createElement('p');
+    headerLocation.classList.add('shows__header');
+    headerLocation.textContent = 'LOCATION';
+
     const showLocation = document.createElement('p');
     showLocation.classList.add('location');
     showLocation.textContent = showData[i].location;
 
-    const button = document.createElement("button");
-        button.innerText = "BUY TICKETS";
+    locationContainer.appendChild(headerLocation);
+    locationContainer.appendChild(showLocation);
 
-    showContainer.appendChild(date);
-    showContainer.appendChild(venue);
-    showContainer.appendChild(showLocation);
+    const button = document.createElement("button");
+    button.innerText = "BUY TICKETS";
+    
+    showContainer.appendChild(dateContainer);
+    showContainer.appendChild(venueContainer);
+    showContainer.appendChild(locationContainer);
     showContainer.appendChild(button);
 
-
-button.addEventListener('click',()=> {
-    console.log('someone bought a ticket');
-});
-
-allShows.appendChild(showContainer);
-}
-
-console.log(allShows);
-
+    button.addEventListener('click',()=> {
+        console.log('someone bought a ticket');
+    });
+    
+    allShows.appendChild(showContainer);
+    }
+    
+    console.log(allShows);

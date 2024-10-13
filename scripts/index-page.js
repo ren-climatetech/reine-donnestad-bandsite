bb9a5b609d6f1dc3c970fafbef9081629454f565
Ren <reine.donnestad@gmail.com>
2024-10-13 15:54:47 -0400

    coded comment section in JavaScript with some styling

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,14 +1,35 @@
+
+//Step 1: Create local data
 const commentEntries = [
     {name: "Victor Pinto", date:"11/02/2023", comment:"This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."},
     {name:"Christina Cabrera", date: "10/28/2023", comment: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."},
     {name:"Isaac Tadesse", date: "10/20/2023", comment: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."}
-]
-
+];
 console.log(commentEntries);
 
+//Step 2: Create a function to render HTML elements. Parent element is class "comment__card" with three children: 1) "comment__card--name" 2) "comment__card--date" and 3) comment__card--comment, where "(entry)" is the object. 
+
 function createCommentCard(entry) {
+
     const cardElement = document.createElement("article");
     cardElement.classList.add("comment__card");
+
+    const imageContainer = document.createElement("div");
+    imageContainer.classList.add("comment__image--container");
+
+    const cardImage = document.createElement("img");
+    cardImage.classList.add("comment__image--empty");
+    cardImage.src = entry.image || "assets/images/Mohan-muruge.jpg"; 
+
+
+    imageContainer.appendChild(cardImage);
+
+
+    const positionDiv = document.createElement("div");
+    positionDiv.classList.add("comment__position");
+
+    const nameWithDateDiv = document.createElement("div");
+    nameWithDateDiv.classList.add("comment__nameWithDate");
 
     const pTag = document.createElement("p");
     pTag.classList.add("comment__card--name");
@@ -18,13 +39,19 @@ function createCommentCard(entry) {
     dateElement.classList.add("comment__card--date");
     dateElement.innerText = entry.date;
 
-    const commentElement = document.createElement("p");
-    pTag.classList.add("comment__card--comment");
-    pTag.innerText = entry.comment;
+    nameWithDateDiv.appendChild(pTag);
+    nameWithDateDiv.appendChild(dateElement);
 
-    cardElement.appendChild(pTag);
-    cardElement.appendChild(dateElement);
-    cardElement.appendChild(commentElement);
+    const commentElement = document.createElement("p");
+    commentElement.classList.add("comment__card--comment");
+    commentElement.innerText = entry.comment;
+
+    positionDiv.appendChild(nameWithDateDiv);
+    positionDiv.appendChild(commentElement);
+
+
+    cardElement.appendChild(imageContainer);
+    cardElement.appendChild(positionDiv);
 
     console.log(cardElement);
     return cardElement;
@@ -43,20 +70,31 @@ let renderCommentCards = () => {
 
 renderCommentCards();
 
-const formEl = document.querySelector(".commentforum");
-formEl.addEventListener("submit",(event) => {
-    event.preventDefault();
+//Method for Comment Button Action
 
-    let cardData = {
-        name: event.target.name.value,
-        date:event.target.date.value,
-        comment:event.target.comment.value,
-    };
-commentEntries.push(cardData);
-console.log(commentEntries);
+document.addEventListener("DOMContentLoaded", () => {
 
-renderCommentCards();
+    const formEl = document.querySelector(".comment");
 
+    formEl.addEventListener("submit",(event) => {
+        event.preventDefault();
+
+        let cardData = {
+            name: event.target.userName.value,
+            date: new Date ().toLocaleDateString(),
+            comment: event.target.userComment.value,
+            // image: event.target.image.value || "",
+        };
+    commentEntries.push(cardData);
+    console.log(commentEntries);
+
+    const newCommentCard = createCommentCard(cardData);
+    const commentsContainer = document.querySelector(".comment-container");
+    commentsContainer.appendChild(newCommentCard);
+
+
+    formEl.reset();
+    });
 });
 
 
@@ -124,5 +162,4 @@ renderCommentCards();
 //         date: date,
 //         comment: comment,
 //     };
-// });
-// });
+
